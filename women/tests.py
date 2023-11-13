13bd3a34dd712789b7466736cfb606dc8e39579c
qnn4ik <qnn4ik@mail.ru>
2023-11-13 13:22:07 +0300

    initial commit

---
+++ women/tests.py
@@ -0,0 +1,27 @@
+from http import HTTPStatus
+
+from django.test import TestCase
+from django.urls import reverse
+
+
+class GetPagesTestCase(TestCase):
+    def setUp(self):
+        "Инициализация перед выполнением каждого теста"
+
+    def test_mainpage(self):
+        path = reverse('home')
+        response = self.client.get(path)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        # self.assertIn('women/index.html', response.template_name)
+        self.assertTemplateUsed(response, 'women/index.html')
+        self.assertEqual(response.context_data['title'], "Главная страница")
+
+    def test_redirect_addpage(self):
+        path = reverse('add_page')
+        redirect_uri = reverse('users:login') + '?next=' + path
+        response = self.client.get(path)
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        self.assertRedirects(response, redirect_uri)
+
+    def tearDown(self):
+        "Действия после выполнения каждого теста"
