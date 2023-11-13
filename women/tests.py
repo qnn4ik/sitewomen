1e5cdb701486b8ad1b1950c8d8a3ceb9db18d08a
qnn4ik <qnn4ik@mail.ru>
2023-11-13 15:25:23 +0300

    added tests for main_page and add_page

--- women/tests.py
+++ women/tests.py
@@ -1,27 +1,28 @@
 from http import HTTPStatus
 
-from django.test import TestCase
 from django.urls import reverse
+from django.test import TestCase
 
 
-class GetPagesTestCase(TestCase):
+class GetPageTestCase(TestCase):
     def setUp(self):
-        "Инициализация перед выполнением каждого теста"
+        """Инициализация перед выполнением каждого теста"""
 
     def test_mainpage(self):
         path = reverse('home')
-        response = self.client.get(path)
-        self.assertEqual(response.status_code, HTTPStatus.OK)
-        # self.assertIn('women/index.html', response.template_name)
-        self.assertTemplateUsed(response, 'women/index.html')
-        self.assertEqual(response.context_data['title'], "Главная страница")
+        response = self.client.get(path)  # imitating GET query to `path`
+
+        self.assertEqual(response.status_code, HTTPStatus.OK)  # check status code
+        self.assertTemplateUsed(response, 'women/index.html')  # check response templates contain `index.html`
+        self.assertEqual(response.context_data['title'], 'Главная страница')
 
     def test_redirect_addpage(self):
         path = reverse('add_page')
         redirect_uri = reverse('users:login') + '?next=' + path
         response = self.client.get(path)
+
         self.assertEqual(response.status_code, HTTPStatus.FOUND)
-        self.assertRedirects(response, redirect_uri)
+        self.assertRedirects(response, redirect_uri)  # check response redirects to redirect_uri
 
     def tearDown(self):
-        "Действия после выполнения каждого теста"
+        """Действия после выполнением каждого теста, например, очистка ресурсов"""
